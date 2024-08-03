90eadc5442d095233648d181d3179cac6d91d67b
Luis Carlos <luiscbravo94@gmail.com>
2024-08-03 19:53:58 +0100

    logic almost finished..

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-console.log("funciona");
-console.log("long live the king");
-console.log('Tercer Commit')
+const textArea = document.getElementById('textarea')
+const list = document.getElementById('list')
+const addWord = []
+
+let words = ''
+
+textArea.addEventListener('keyup', (e) =>{
+    
+    // Obtenemos el valor del textArea
+    words = e.target.value.trim()
+    list.innerHTML = words.split(',').map(word => `<li class='list-item'>${word}</li>`).join('')
+})
+
+
